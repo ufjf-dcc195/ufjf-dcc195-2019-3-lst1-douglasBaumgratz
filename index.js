52826f6bdf037e5d4b943a476127c38cce7e5492
douglasBaumgratz <douglas.baumgratz@gmail.com>
2019-09-03 23:16:04 -0300

    Criando todas views e rotas do sistema

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ var handlers = {};
 handlers["/"] = requestHandlers.index;
 handlers["/index.html"] = requestHandlers.index;
 handlers["/sobre.html"] = requestHandlers.sobre;
+handlers["/aleatorios.html"] = requestHandlers.aleatorios;
+handlers["/primos.html"] = requestHandlers.primos;
+handlers["/equacao.html"] = requestHandlers.equacao;
+handlers["/xadrez.html"] = requestHandlers.xadrez;
+handlers["/xadrez.json"] = requestHandlers.xadrez;
 
 server.start(router.route, handlers);
--- requestHandlers.js
+++ requestHandlers.js
@@ -17,7 +17,7 @@ function index(request, response) {
 function sobre(request, response) {
     if (request.method == "GET") {
         response.writeHead(200, { "Content-Type": "text/html" });
-        response.write("<h3> Sobre </h3>");        
+        response.write("<h3> Sobre </h3>");
         response.write("<label> Nome: Douglas Baumgratz de Carvalho </label><br/>");
         response.write("<label> Número de matrícula: 201276007 </label><br/>");
         response.write("<label> Curso: Sistemas de Informação </label><br/>");
@@ -27,6 +27,49 @@ function sobre(request, response) {
 }
 
 
+function aleatorios(request, response) {
+    if (request.method == "GET") {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write("<h3> Aleatórios </h3>");
+
+        response.write("<a href='index.html'>Voltar</a> \n");
+        response.end();
+    }
+}
+
+function primos(request, response) {
+    if (request.method == "GET") {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write("<h3> Primos </h3>");
+
+        response.write("<a href='index.html'>Voltar</a> \n");
+        response.end();
+    }
+}
+
+function equacao(request, response) {
+    if (request.method == "GET") {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write("<h3> Equação </h3>");        
+
+        response.write("<a href='index.html'>Voltar</a> \n");
+        response.end();
+    }
+}
+
+function xadrez(request, response) {
+    if (request.method == "GET") {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write("<h3> Xadrez </h3>");        
+
+        response.write("<a href='index.html'>Voltar</a> \n");
+        response.end();
+    }
+}
 exports.index = index;
 exports.sobre = sobre;
+exports.aleatorios = aleatorios;
+exports.primos = primos;
+exports.equacao = equacao;
+exports.xadrez = xadrez;
 
