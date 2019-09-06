d049e6a4202a52bb0f1e113709d25d28fe5bc5c2
douglasBaumgratz <douglas.baumgratz@gmail.com>
2019-09-05 22:29:37 -0300

    Heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ function start(route, handlers) {
             response
         )
     }
-    http.createServer(onRequest).listen(8888)
+    http.createServer(onRequest).listen(process.env.PORT)
     console.log("Servidor iniciado em localhost:8888")
 }
 exports.start = start
